534fc1f3abad5b9a2da79ea1844fc83875407306
u-phoria <uros.rapajic@gmail.com>
2019-09-15 14:12:05 +0100

    push models script

---
+++ project/push_models.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -euxo pipefail
+
+EC2_PK="~/ur-mbp-aws.pem"
+EC2_IP="54.236.51.178"
+REMOTE_PATH_ROOT="/home/ubuntu/natural-language-processing/project"
+
+for SRC_PATH in 'tfidf_vectorizer.pkl' 'intent_recognizer.pkl' 'tag_classifier.pkl'; do
+    scp -i $EC2_PK $SRC_PATH ubuntu@$EC2_IP:$REMOTE_PATH_ROOT/$SRC_PATH
+done
+
+scp -i $EC2_PK -rp thread_embeddings_by_tags ubuntu@$EC2_IP:$REMOTE_PATH_ROOT/thread_embeddings_by_tags
